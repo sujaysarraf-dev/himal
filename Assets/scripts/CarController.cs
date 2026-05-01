446f4509009aa22f18f07d6b1af0a1866a90db24
sujaysarraf-dev <developersujaynp@gmail.com>
2026-05-02 01:49:21 +0545

    Remove zebra logic from CarController

--- Assets/scripts/CarController.cs
+++ Assets/scripts/CarController.cs
@@ -5,11 +5,8 @@ public class CarController : MonoBehaviour
 {
     public Transform[] wheelMeshes;
     public float speed = 10f;
-    public float stopDistance = 15f;
 
     private Rigidbody rb;
-    private ZebraCrossing detectedZebra;
-    private bool isStopped = false;
 
     void Start()
     {
@@ -31,82 +28,12 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
-        DetectZebraCrossing();
-
-        if (!isStopped)
+        if (rb != null)
         {
-            if (rb != null)
-            {
-                float currentYVelocity = rb.linearVelocity.y;
-                rb.linearVelocity = new Vector3(transform.forward.x * speed, currentYVelocity, transform.forward.z * speed);
-            }
-            RotateWheels();
+            float currentYVelocity = rb.linearVelocity.y;
+            rb.linearVelocity = new Vector3(transform.forward.x * speed, currentYVelocity, transform.forward.z * speed);
         }
-        else
-        {
-            if (rb != null)
-            {
-                rb.linearVelocity = Vector3.zero;
-            }
-        }
-    }
-
-    void DetectZebraCrossing()
-    {
-        RaycastHit hit;
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.5f;
-
-        Debug.Log("Raycasting forward from " + gameObject.name + " at position " + transform.position);
-
-        if (Physics.Raycast(rayOrigin, transform.forward, out hit, stopDistance))
-        {
-            Debug.Log("Ray hit: " + hit.collider.name + " tag: " + hit.collider.tag);
-
-            ZebraCrossing zebra = hit.collider.GetComponent<ZebraCrossing>();
-            if (zebra == null) zebra = hit.collider.GetComponentInParent<ZebraCrossing>();
-
-            Debug.Log("ZebraCrossing found: " + (zebra != null));
-
-            if (zebra != null)
-            {
-                if (detectedZebra != zebra)
-                {
-                    detectedZebra = zebra;
-                    Debug.Log("Car " + gameObject.name + " detected zebra: " + zebra.gameObject.name);
-                }
-
-                Debug.Log("isPedestrianCrossing: " + zebra.isPedestrianCrossing);
-
-                if (zebra.isPedestrianCrossing)
-                {
-                    if (!isStopped)
-                    {
-                        isStopped = true;
-                        Debug.Log("Car " + gameObject.name + " STOPPED");
-                    }
-                }
-                else
-                {
-                    if (isStopped)
-                    {
-                        isStopped = false;
-                        Debug.Log("Car " + gameObject.name + " GOING");
-                    }
-                }
-            }
-        }
-        else
-        {
-            Debug.Log("Ray hit nothing");
-            if (isStopped)
-            {
-                isStopped = false;
-                Debug.Log("Car " + gameObject.name + " GOING - no zebra");
-            }
-            detectedZebra = null;
-        }
-
-        Debug.DrawRay(rayOrigin, transform.forward * stopDistance, isStopped ? Color.red : Color.green);
+        RotateWheels();
     }
 
     void FindWheelMeshes()
